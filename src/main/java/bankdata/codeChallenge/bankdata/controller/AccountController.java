e988bcd33d38829385d2d6b5e13758863bcbaaf5
Morten Meyer Rasmussen <mortenm12@hotmail.com>
2025-06-05 12:30:31 +0200

    adding response and error handling

--- src/main/java/bankdata/codeChallenge/bankdata/controller/AccountController.java
+++ src/main/java/bankdata/codeChallenge/bankdata/controller/AccountController.java
@@ -4,6 +4,7 @@ import bankdata.codeChallenge.bankdata.model.Account;
 import bankdata.codeChallenge.bankdata.service.AccountService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,43 +14,42 @@ public class AccountController {
     @Autowired
     AccountService accountService;
 
-    //I didn't get around to use response instead og account or double as the return of the requests, but that would be more elegant and helps with handling errors
-
     @PostMapping("/create")
-    public Account createAccount(@RequestParam String firstName, @RequestParam String lastName) {
+    public ResponseEntity<Account> createAccount(@RequestParam String firstName, @RequestParam String lastName) {
         Account account = new Account(firstName, lastName);
         accountService.saveAccount(account);
-        return account;
+        return ResponseEntity.ok(account);
     }
 
     @GetMapping("/{id}")
-    public Account getAccount(@PathVariable("id") String id) {
+    public ResponseEntity<Account> getAccount(@PathVariable("id") String id) {
         int accountNumber = Integer.parseInt(id);
-        return accountService.findByAccountNumber(accountNumber);
+        return ResponseEntity.ok(accountService.findByAccountNumber(accountNumber));
     }
 
     @GetMapping("/{id}/balance")
-    public double getBalance(@PathVariable("id") String id) {
+    public ResponseEntity<Double> getBalance(@PathVariable("id") String id) {
         int accountNumber = Integer.parseInt(id);
-        return accountService.findByAccountNumber(accountNumber).getBalance();
+        return ResponseEntity.ok(accountService.findByAccountNumber(accountNumber).getBalance());
     }
 
     @PutMapping("/{id}/deposit")
-    public double deposit(@PathVariable("id") String id, @RequestParam String amount) {
+    public ResponseEntity<Double> deposit(@PathVariable("id") String id, @RequestParam String amount) {
         double amountDouble = Double.parseDouble(amount);
         int accountNumber = Integer.parseInt(id);
         Account account = accountService.findByAccountNumber(accountNumber);
         accountService.deposit(account, amountDouble);
-        return account.getBalance();
+        return ResponseEntity.ok(account.getBalance());
     }
 
     @PutMapping("/transfer")
-    public void transfer(@RequestParam String idFrom, @RequestParam String idTo, @RequestParam String amount) {
+    public ResponseEntity<String> transfer(@RequestParam String idFrom, @RequestParam String idTo, @RequestParam String amount) {
         double amountDouble = Double.parseDouble(amount);
         int accountNumberFrom = Integer.parseInt(idFrom);
         int accountNumberTo = Integer.parseInt(idTo);
         Account accountFrom = accountService.findByAccountNumber(accountNumberFrom);
         Account accountTo = accountService.findByAccountNumber(accountNumberTo);
         accountService.transfer(accountFrom, accountTo, amountDouble);
+        return ResponseEntity.ok("");
     }
 }
--- src/main/java/bankdata/codeChallenge/bankdata/controller/ExchangeController.java
+++ src/main/java/bankdata/codeChallenge/bankdata/controller/ExchangeController.java
@@ -3,12 +3,14 @@ package bankdata.codeChallenge.bankdata.controller;
 import bankdata.codeChallenge.bankdata.service.ExchangeService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Nullable;
 import org.joda.time.DateTime;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,9 +19,10 @@ import java.util.Map;
 @Tag(name = "Exchange")
 @RequestMapping("exchange")
 public class ExchangeController {
+    Logger logger = LoggerFactory.getLogger(ExchangeController.class);
 
     @GetMapping("/{currency}")
-    String getExchangeRate (@PathVariable("currency") String currency) {
+    ResponseEntity<String> getExchangeRate (@PathVariable("currency") String currency) {
         ExchangeService exchangeService = new ExchangeService();
         double amount = exchangeService.exchange(100, currency);
         Map<String, Double> result = new HashMap<>();
@@ -27,27 +30,43 @@ public class ExchangeController {
         result.put(currency, amount);
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            return objectMapper.writeValueAsString(result);
+            return ResponseEntity.ok(objectMapper.writeValueAsString(result));
         } catch (JsonProcessingException e) {
-            return "error"; //better to use a response
+            logger.error(e.getMessage());
+            return ResponseEntity.internalServerError().build();
         }
     }
 
+    //needs more api description of the format for datetime and response bad request give a 400 instead of
     @GetMapping("/history/{currency}/{dateFrom}/{dateTo}")
-    String getExchangeHistory (@PathVariable("currency") String currency, @PathVariable("dateFrom") String dateFrom, @PathVariable("dateTo") String dateTo) {
-        //remember to make exclude
-        //remember to make gradient
+    ResponseEntity<String> getExchangeHistory (@PathVariable("currency") String currency,
+                               @PathVariable("dateFrom") @Parameter(description = "format: yyyy-mm-dd") String dateFrom,
+                               @PathVariable("dateTo") @Parameter(description = "format: yyyy-mm-dd")String dateTo,
+                               @Nullable @RequestParam("excludeFrom") @Parameter(description = "format: yyyy-mm-dd") String excludeFrom,
+                               @Nullable @RequestParam("excludeTo") @Parameter(description = "format: yyyy-mm-dd") String excludeTo) {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             ExchangeService exchangeService = new ExchangeService();
-            DateTime dateTimeFrom = DateTime.parse(dateFrom);
-            DateTime dateTimeTo = DateTime.parse(dateTo);
-            if(dateTimeFrom.isAfter(dateTimeTo)){
-                return "error"; //handle response status
+            DateTime dateTimeFrom = dateFrom != null ? DateTime.parse(dateFrom) : null;
+            DateTime dateTimeTo = dateTo != null ? DateTime.parse(dateTo) : null;
+            DateTime dateTimeExcludeFrom = excludeFrom != null ? DateTime.parse(excludeFrom) : null;
+            DateTime dateTimeExcludeTo = excludeTo != null ? DateTime.parse(excludeTo) : null;
+            if(dateTimeFrom == null || dateTimeTo == null){
+                logger.warn("dateTime are not correct");
+                return ResponseEntity.badRequest().body("dateTime are not correct");
             }
-            return objectMapper.writeValueAsString(exchangeService.exchangeHistory(100, currency, dateTimeFrom, dateTimeTo));
+            if(dateTimeFrom.isAfter(dateTimeTo)){
+                logger.warn("dateFrom is after dateTo");
+                return ResponseEntity.badRequest().body("dateFrom is after dateTo");
+            }
+            if(dateTimeExcludeFrom != null && dateTimeExcludeTo != null && dateTimeExcludeFrom.isAfter(dateTimeExcludeTo)){
+                logger.warn("excludeDateFrom is after excludeDateTo");
+                return ResponseEntity.badRequest().body("excludeDateFrom is after excludeDateTo");
+            }
+            return ResponseEntity.ok(objectMapper.writeValueAsString(exchangeService.exchangeHistory(100, currency, dateTimeFrom, dateTimeTo, dateTimeExcludeFrom, dateTimeExcludeTo)));
         } catch (JsonProcessingException e) {
-            return "error"; //better to use a response
+            logger.error(e.getMessage());
+            return ResponseEntity.internalServerError().build();
         }
     }
 }
